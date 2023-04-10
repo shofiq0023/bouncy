c862bb06900c002b72bce243dd132a7e457332d5
shofiqul <acms@acms.acms>
2023-04-10 12:16:42 +0600

    added door opening mechanism

--- Assets/Scripts/GateScript.cs
+++ Assets/Scripts/GateScript.cs
@@ -1,15 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class GateScript : MonoBehaviour {
+    private const string GATE_OPEN_TRIGGER = "GateOpenTrigger";
+
+    private CinemachineVirtualCamera cinemachineVCam;
+
+    [SerializeField] private Collider2D gateCollider;
+    [SerializeField] private Animator gateAnimator;
+
+    private void Start() {
+        cinemachineVCam = GetComponentInChildren<CinemachineVirtualCamera>();
+    }
+
     public void OpenGate() {
-        Debug.Log("Gate open!");
-        // Create gate open animation
-        // Active the camera
-        // Coroutine for 1.5 second (Duration of opening gate animation)
-            // Inside coroutine play the gate open animation
-            // Disable the collider
-        // Deactive the camera
+        StartCoroutine(IOpenGame());
+    }
+
+    IEnumerator IOpenGame() {
+        // Puase the game
+        Time.timeScale = 0;
+
+        // Active the virtual camera
+        cinemachineVCam.enabled = true;
+        yield return new WaitForSecondsRealtime(1.8f);
+
+        // Play the gate opening animation
+        gateAnimator.SetTrigger(GATE_OPEN_TRIGGER);
+        yield return new WaitForSecondsRealtime(0.3f);
+
+        // Disable the gate collider and disable the virtual camera
+        gateCollider.enabled = false;
+        cinemachineVCam.enabled = false;
+        yield return new WaitForSecondsRealtime(1.8f);
+
+        // Resume the game
+        Time.timeScale = 1;
     }
 }
--- Assets/Scripts/GateSwitch.cs
+++ Assets/Scripts/GateSwitch.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GateSwitch : MonoBehaviour {
+    private const string SWITCH_ON_TRIGGER = "SwitchOn";
+
     private BoxCollider2D boxCollider;
     private GateScript gate;
     private SpriteRenderer switchSprite;
+    private Animator switchAnimator;
     
     [SerializeField] private BoxCollider2D childCollider;
 
@@ -13,9 +16,11 @@ public class GateSwitch : MonoBehaviour {
         boxCollider = GetComponent<BoxCollider2D>();
         gate = GetComponentInParent<GateScript>();
         switchSprite = GetComponentInChildren<SpriteRenderer>();
+        switchAnimator = GetComponent<Animator>();
     }
 
     private void OnCollisionEnter2D(Collision2D collider) {
+        switchAnimator.SetTrigger(SWITCH_ON_TRIGGER);
         boxCollider.enabled = false;
         switchSprite.color = Color.green;
         gate.OpenGate();
